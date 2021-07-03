f426a5da35bfa138a01db7e7d16a67b3d5f8a7e8
Hanif Eridaputra <hanif.eridaputra@mhealth.tech>
2021-07-03 17:19:06 +0700

    import jsonSchema

--- main.py
+++ main.py
@@ -6,12 +6,41 @@
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 import requests
+import json
+from jsonschema import validate
+
+numOfTest = 0
+numOfPass = 0
+numOfFail = 0
 
 query = "query product($productId: String!) {\n product(productId: $productId) {\n ... on Product {\n productId\n " \
-        "productName\n destinationId\n shortDescription\n cancellationType\n cancellationWindow\n minGroupSize\n " \
-        "openDateTicket\n collectPhysicalTicket\n confirmationType\n voucherType\n priceRangeFrom\n " \
-        "priceRangeTo\n latitude\n longitude\n address\n __typename\n }\n ... on PelagoError {\n errorMessage\n " \
-        "code\n __typename\n }\n __typename\n }\n} "
+        "productName\n destinationId\n mediaData\n cancellationType\n cancellationWindow\n minGroupSize\n duration\n " \
+        "openDateTicket\n collectPhysicalTicket\n confirmationType\n voucherType\n guideLanguages\n priceRangeFrom\n " \
+        "priceRangeTo\n latitude\n longitude\n address\n }\n ... on PelagoError {\n errorMessage\n " \
+        "code\n }\n }\n} "
+
+productSchema = {
+    "type": "object",
+    "properties": {
+        "productId": {"type": "string"},
+        "productName": {"type": "string"},
+        "destinationId": {"type": "string"},
+        "cancellationType": {"type": "string"},
+        "cancellationWindow": {"type": "number"},
+        "minGroupSize": {"type": ["number", "null"]},
+        "duration": {"type": "string"},
+        "openDateTicket": {"type": "boolean"},
+        "collectPhysicalTicket": {"type": "boolean"},
+        "confirmationType": {"type": "string"},
+        "voucherType": {"type": "string"},
+        "guideLanguages": {"type": "string"},
+        "priceRangeFrom": {"type": "number"},
+        "priceRangeTo": {"type": "number"},
+        "latitude": {"type": "string"},
+        "longitude": {"type": "string"},
+        "address": {"type": "string"},
+    },
+}
 
 
 def test_first():
@@ -20,7 +49,10 @@ def test_first():
     # print(response.json())
     response_body = response.json()
     assert response.status_code == 200
-    assert response_body['data']['product']['destinationId'] == "singapore", "destinationId is not singapore"
+    # assert response_body['data']['product']['destinationId'] == "singapore", "destinationId is not singapore"
+    productResponse = response_body['data']['product']
+    isValid = validate(instance=productResponse, schema=productSchema)
+    print("Test first is passed")
 
 
 if __name__ == '__main__':
